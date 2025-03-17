c4383d77b915dd8c1cece8e34024d17b01a443b5
GeoffreyAntonio19 <geo.arifin@gmail.com>
2025-03-17 14:15:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ def main():
   st.write('This app using Machine Learning')
 
   # input data by user
-  erytghema = st.slider('Erythema', min_value=0, max_value=3, value=2)
+  erythema = st.slider('Erythema', min_value=0, max_value=3, value=2)
   
 if __name__ == "__main__":
   main()
