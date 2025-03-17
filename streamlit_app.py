6770ad1adbc25eb90f2fe1f8658100f6a255a6eb
GeoffreyAntonio19 <geo.arifin@gmail.com>
2025-03-17 14:11:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import joblib
 
-st.title('Dermatology Machine Learning')
+def main():
+  st.title('Dermatology Machine Learning')
+  st.write('This app using Machine Learning')
 
-st.write('This app using Machine Learning')
+
+if __name__ == "__main__":
+  main()
+  
