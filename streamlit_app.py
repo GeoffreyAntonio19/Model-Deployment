b3351990aaf46c4a730f108a8ad7c4ed4019cda8
GeoffreyAntonio19 <geo.arifin@gmail.com>
2025-03-17 14:15:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,9 @@ def main():
   st.title('Dermatology Machine Learning')
   st.write('This app using Machine Learning')
 
-
+  # input data by user
+  erytghema = st.slider('Erythema', min_value=0, max_value=3, value=2)
+  
 if __name__ == "__main__":
   main()
   
